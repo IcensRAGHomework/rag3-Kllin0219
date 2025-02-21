2c05f2bb6584a74e639894aad0248c9468bfd471
guoliang_lin <guoliang_lin@asus.com>
2025-02-21 10:55:34 +0800

    generate_hw02

--- student_assignment.py
+++ student_assignment.py
@@ -15,6 +15,7 @@ gpt_emb_config = get_model_configuration(gpt_emb_version)
 dbpath = "./"
 csv_file = "COA_OpenData.csv"
 
+
 def generate_hw01():
     chroma_client = chromadb.PersistentClient(path=dbpath)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
@@ -56,7 +57,55 @@ def generate_hw01():
 
 
 def generate_hw02(question, city, store_type, start_date, end_date):
-    pass
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=gpt_emb_config['api_key'],
+        api_base=gpt_emb_config['api_base'],
+        api_type=gpt_emb_config['openai_type'],
+        api_version=gpt_emb_config['api_version'],
+        deployment_id=gpt_emb_config['deployment_name']
+    )
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+    
+    where_conditions = []
+    if city:
+        if isinstance(city, list) and len(city) > 1:
+            where_conditions.append({"$or": [{"city": c} for c in city]})
+        else:
+            where_conditions.append(
+                {"city": city[0] if isinstance(city, list) else city})
+    if store_type:
+        if isinstance(store_type, list) and len(store_type) > 1:
+            where_conditions.append({"$or": [{"type": t} for t in store_type]})
+        else:
+            where_conditions.append(
+                {"type": store_type[0] if isinstance(store_type, list) else store_type})
+    if start_date:
+        where_conditions.append(
+            {"date": {"$gte": int(start_date.timestamp())}})
+    if end_date:
+        where_conditions.append(
+            {"date": {"$lte": int(end_date.timestamp())}})  # 小於等於 end_date
+
+    where_filter = {"$and": where_conditions} if where_conditions else None
+    # print(where_filter)
+
+    results = collection.query(
+        query_texts=[question],
+        n_results=10,
+        where= where_filter)
+    
+    filtered_results = []
+    for doc, meta, score in zip(results["documents"][0], results["metadatas"][0], results["distances"][0]):
+        if score < 0.2:  
+            filtered_results.append((meta.get("name"), score))
+
+    filtered_results.sort(key=lambda x: x[1])  
+    return [name for name, _ in filtered_results]
 
 
 def generate_hw03(question, store_name, new_store_name, city, store_type):
@@ -82,6 +131,10 @@ def demo(question):
 
 
 if __name__ == '__main__':
-    collection = generate_hw01()
-    data = collection.get(limit=1, include=["embeddings", "documents", "metadatas"])
-    print(data)
+    # collection = generate_hw01()
+    # data = collection.get(
+    #     limit=1, include=["embeddings", "documents", "metadatas"])
+    # print(data['embeddings'].shape)
+    result = generate_hw02("我想要找有關茶餐點的店家", ["宜蘭縣", "新北市"], ["美食"], datetime.datetime(
+        2024, 4, 1), datetime.datetime(2024, 5, 1))
+    print(result)
